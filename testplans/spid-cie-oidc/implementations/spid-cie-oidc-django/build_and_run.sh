7f7c56910e8f302e786e730ba7507cda85304185
pdematteis <pdematteis@fbk.eu>
2024-10-07 15:22:00 +0200

    fixed scripts to run in github workflow

--- testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
+++ testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
@@ -4,7 +4,9 @@ cd $(dirname "$0") # Go to directory containing script
 
 git clone https://github.com/italia/spid-cie-oidc-django.git
 rm ./spid-cie-oidc-django/docker-compose.yml
+cp ./edited_files/docker-compose-base.yml ./spid-cie-oidc-django/
 cp ./edited_files/docker-compose.yml ./spid-cie-oidc-django/
+cp ./edited_files/docker-compose-headless.yml ./spid-cie-oidc-django/
 rm ./spid-cie-oidc-django/Dockerfile
 cp ./edited_files/Dockerfile ./spid-cie-oidc-django/
 cd spid-cie-oidc-django
@@ -18,13 +20,13 @@ sudo docker build -t i-mig-t .
 cd ../../testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/spid-cie-oidc-django/
 # local build i-mig-t --------
 
+echo "HEADLESS_MODE $HEADLESS_MODE"
 if [ $HEADLESS_MODE ]; 
 then 
   # mandatory: use "detached" mode (-d option) for pipeline
-  sudo docker compose up -d --remove-orphans
+  sudo docker compose -f docker-compose-headless.yml up -d --remove-orphans
   wait
 else
-  xhost not available in a pipeline context
   xhost +local:
   sudo docker compose up --remove-orphans
   wait
